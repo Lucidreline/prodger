94c29fe40646e07eddb16aba2d5cf8b7f447be74
Lucidreline <techservs@pm.me>
2020-06-10 18:38:57 -0700

    Added buttons to control stopwatch

--- src/components/pages/stopwatch-page/stopwatch-page.component.jsx
+++ src/components/pages/stopwatch-page/stopwatch-page.component.jsx
@@ -8,6 +8,7 @@ class StopwatchPage extends Component {
     super();
     this.state = {
       seconds: 0,
+      paused: true,
     };
   }
 
@@ -15,20 +16,59 @@ class StopwatchPage extends Component {
     return (
       <div className='stopwatch-page'>
         <StopWatch seconds={this.state.seconds} />
+        {this.whichBtnsToShow()}
       </div>
     );
   }
 
-  componentDidMount() {
+  whichBtnsToShow = () => {
+    // handles what buttons show up
+    if (this.state.seconds === 0 && this.state.paused) {
+      return <button onClick={this.resumeStopwatch}>Start</button>;
+    } else if (this.state.paused) {
+      return (
+        <div className='paused-btns'>
+          <button onClick={this.resumeStopwatch}>Resume</button>
+          <button onClick={this.resetStopwatch}>Reset</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className='unpaused-btns'>
+          <button onClick={this.pauseStopwatch}>Pause</button>
+          <button onClick={this.resetStopwatch}>Reset</button>
+        </div>
+      );
+    }
+  };
+
+  resumeStopwatch = () => {
+    //this first setState is to remove the 1 second lag when the user hit resume
+    this.setState(() => ({
+      paused: false,
+    }));
+
     this.myInterval = setInterval(() => {
       this.setState(prevState => ({
         seconds: prevState.seconds++,
       }));
     }, 1000);
-  }
+  };
+
+  pauseStopwatch = () => {
+    this.setState({ paused: true }, () => {
+      clearInterval(this.myInterval);
+    });
+  };
+
+  resetStopwatch = () => {
+    this.setState({ seconds: 0, paused: true }, () => {
+      clearInterval(this.myInterval);
+    });
+  };
 
   componentWillUnmount() {
-    clearInterval(this.myInterval);
+    this.resetStopwatch();
   }
 }
 
